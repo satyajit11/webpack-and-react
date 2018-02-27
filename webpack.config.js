1cfa346fc636f4e5cde1f17d06dbbe57d84c116f
Satyajit Rout <satyajitrout@Satyajits-MacBook-Pro-2.local>
2018-02-28 00:09:31 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build')
+};
+
+const common = {
+    entry: {
+        app: PATHS.app
+    },
+    output: {
+        path: PATHS.build,
+        filename: 'bundle.js'
+    }
+};
+
+if(TARGET === 'start' || !TARGET){
+    module.exports = merge(common, {});
+}
+if(TARGET === 'build'){
+    module.exports = merge(common, {});
+}
